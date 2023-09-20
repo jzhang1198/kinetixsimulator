7a97875a9f6dfcf8a45cf5db9dc28ca0ee92c931
jzhang1198 <jzhang1198@gmail.com>
2023-09-20 08:08:56 -0700

    Explicit reference to python version requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Python package for interactive simulation of kinetic mechanisms.',
     url='https://github.com/jzhang1198/kinetixsimulator',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
